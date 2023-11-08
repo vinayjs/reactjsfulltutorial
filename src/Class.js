23c5f0e88a2b6c844e43a8eafc1c083fbd20975a
vinay chandran <vinay.sarvodaya1991@gmail.com>
2023-11-08 17:51:50 +0530

    props in class components

--- src/Class.js
+++ src/Class.js
@@ -6,10 +6,30 @@ class Class extends React.Component {
         this.state = {color : 'red'}
     }
     render() {
-        
-        return <h2> hi, this is a class component and my state is {this.state.color}</h2> 
+
+        return (
+            <>
+            <h2> hi, this is a class component and my state is {this.state.color}</h2>
+            <Car brand='benz' />
+            </>
+        ) 
     }
 
+}
+
+
+class Car extends React.Component{
+    constructor(props) {
+        super(props);
+        this.brand = this.props.brand;
+    }
+    render() {
+        return (
+            <>
+            <h2>My car is {this.brand}</h2>
+            </>
+        )
+    }
 }
 
 export default Class;
